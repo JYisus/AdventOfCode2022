b87a2bf3bd27fb1d11b68671d877f5907bc94659
Juan Jesús Padrón Hernández <jjph22@gmail.com>
2022-12-07 20:28:25 +0000

    day 03 part 2

--- javascript/day03/day03.js
+++ javascript/day03/day03.js
@@ -19,9 +19,44 @@ const getDuplicatedItemPriority = (rucksack) => {
 const getItemPriority = (item) => {
     return item.charCodeAt() >= 95
         ? item.charCodeAt() - 96
-        : item.charCodeAt() - 65 + 27
+        : item.charCodeAt() - 65 + 27;
+}
+
+const getRepeatedItemInGroup = (item) => {
+    const rucksacks = item.split("\n")
+        .filter(val => val !== "");
+
+    let sum = 0;
+    for(let j = 0; j < rucksacks.length; j+=3) {
+        sum += getRepeatedItemPriority(rucksacks.slice(j, j + 3));
+    }
+    return sum;
+}
+
+const getRepeatedItemPriority = (rucksacks) => {
+    const itemsStored = {};
+
+    for (let i = 0; i < rucksacks[0].length; i++) {
+        const item = rucksacks[0][i];
+        itemsStored[item] = 1;
+    }
+
+    for (let i = 0; i < rucksacks[1].length; i++) {
+        const item = rucksacks[1][i];
+        if (itemsStored[item]) itemsStored[item] = 2;
+        // console.log({item, stored: itemsStored[item]})
+    }
+
+    for (let i = 0; i < rucksacks[2].length; i++) {
+        const item = rucksacks[2][i];
+        if (itemsStored[item] === 2) {
+            return getItemPriority(item);
+        }
+    }
+    return 0;
 }
 
 module.exports = {
 	getDuplicatedItemsPriorities,
+    getRepeatedItemInGroup,
 }
--- javascript/day03/day03.spec.js
+++ javascript/day03/day03.spec.js
@@ -1,4 +1,4 @@
-const { getDuplicatedItemsPriorities } = require("./day03");
+const { getDuplicatedItemsPriorities, getRepeatedItemInGroup } = require("./day03");
 const fs = require("fs");
 
 describe("Day 3: Rucksack Reorganization", () => {
@@ -24,6 +24,37 @@ describe("Day 3: Rucksack Reorganization", () => {
 		it("calculates the priority of the items priorities with real input", () => {
 			const input = fs.readFileSync("./day03/data03.txt").toString();
 			expect(getDuplicatedItemsPriorities(input)).toEqual(7850);
+		});
+	});
+
+	describe.only("Part 2", () => {
+		it("should calculate the item that the 3 elves of the group has", () => {
+			const input = `vJrwpWtwJgWrhcsFMMfFFhFp
+			jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+			PmmdzqPrVvPwwTWBwg`.replace(/\t/g, "");
+			expect(getRepeatedItemInGroup(input)).toEqual(18);
+		});
+
+		it("should calculate the item that the 3 elves of the group has (example 2)", () => {
+			const input = `wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+			ttgJtRGJQctTZtZT
+			CrZsJsPPZsGzwwsLwLmpwMDw`.replace(/\t/g, "");
+			expect(getRepeatedItemInGroup(input)).toEqual(52);
+		});
+
+		it("should calculate the sum of the priorities of the items repeated in 2 groups", () => {
+			const input = `vJrwpWtwJgWrhcsFMMfFFhFp
+			jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+			PmmdzqPrVvPwwTWBwg
+			wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+			ttgJtRGJQctTZtZT
+			CrZsJsPPZsGzwwsLwLmpwMDw`.replace(/\t/g, "");
+			expect(getRepeatedItemInGroup(input)).toEqual(70);
+		});
+
+		it.only("should calculate the sum of the priorities in the real case", () => {
+			const input = fs.readFileSync("./day03/data03.txt").toString();
+			expect(getRepeatedItemInGroup(input)).toEqual(2581);
 		});
 	});
 });
